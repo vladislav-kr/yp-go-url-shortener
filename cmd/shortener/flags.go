db857a66cc44b121389ecca0dc3f8a15c1610a12
vladislav-kr <vakryuchkov@list.ru>
2023-12-04 02:44:41 +0300

    Inc5. Add default values if flag is specified as initial value

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -1,16 +1,31 @@
 package main
 
-import "flag"
+import (
+	"flag"
+)
+
+const (
+	defHost         = ":8080"
+	defRedirectHost = "http://localhost:8080"
+)
 
 func parseFlags(host, redirectHost *string) {
 
 	if len(*host) == 0 {
-		flag.StringVar(host, "a", ":8080", "address and port to run server")
+		flag.StringVar(host, "a", defHost, "address and port to run server")
 	}
 
 	if len(*redirectHost) == 0 {
-		flag.StringVar(redirectHost, "b", "http://localhost:8080", "redirect address")
+		flag.StringVar(redirectHost, "b", defRedirectHost, "redirect address")
 	}
 
 	flag.Parse()
+
+	if len(*host) == 0 {
+		*host = defHost
+	}
+
+	if len(*redirectHost) == 0 {
+		*redirectHost = defRedirectHost
+	}
 }
