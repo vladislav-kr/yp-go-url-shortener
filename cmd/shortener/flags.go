d410e0e85de7567e893205aa327af754670a3add
vladislav-kr <vakryuchkov@list.ru>
2023-12-04 01:17:02 +0300

    Inc5. Add reading env variables, with highest priority

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -4,8 +4,13 @@ import "flag"
 
 func parseFlags(host, redirectHost *string) {
 
-	flag.StringVar(host, "a", ":8080", "address and port to run server")
-	flag.StringVar(redirectHost, "b", "http://localhost:8080", "redirect address")
-	
+	if len(*host) == 0 {
+		flag.StringVar(host, "a", ":8080", "address and port to run server")
+	}
+
+	if len(*redirectHost) == 0 {
+		flag.StringVar(redirectHost, "b", "http://localhost:8080", "redirect address")
+	}
+
 	flag.Parse()
 }
