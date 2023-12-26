bdeecc65ce4c3b84da658e70b8876f7f9e571bde
vladislav-kr <vakryuchkov@list.ru>
2023-12-26 03:50:11 +0300

    вернул проверку на полный путь

--- internal/lib/fileutils/fileutils.go
+++ internal/lib/fileutils/fileutils.go
@@ -6,6 +6,10 @@ func CreateFullPathFromRelative(relativePath string) (fullPath string, err error
 
 	validPath := filepath.FromSlash(relativePath)
 
+	if filepath.IsAbs(validPath) {
+		return validPath, nil
+	}
+
 	dir, err := filepath.Abs("")
 	if err != nil {
 		return "", err
